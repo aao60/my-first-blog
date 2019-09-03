a771e321390f88c733ccb931e37bb42f08ba83ff
Andy Oliva <andres94oliva@gmail.com>
2019-09-03 14:34:03 -0500

    Modified orange color.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lipyv4ux@gd34%)o3ga*!sjep5u2o8m+5ighg-fpy@wt65p$&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
